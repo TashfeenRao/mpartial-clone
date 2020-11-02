bd5ec121142d9403740f702deb076eb1b11073d2
tashfeenrao <raotashfeen25@gmail.com>
2020-11-02 12:00:49 +0500

    compare image styling added

--- src/components/SectionInfo/CompareImage.js
+++ src/components/SectionInfo/CompareImage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { ArrowLeft, ArrowRight } from "react-bootstrap-icons";
 import ImageSlider from "react-image-comparison-slider";
 import "../../css/CompareImage.css";
 import img1 from "../../images/12.jpg";
@@ -7,16 +8,23 @@ import img2 from "../../images/13.jpg";
 
 export default function CompareImage() {
   return (
-    <div className="" style={{ width: 700, height: 450 }}>
-      <ImageSlider
-        image1={img1}
-        image2={img2}
-        customHandle={<Button>move</Button>}
-        sliderWidth={3}
-        sliderColor="red"
-        handleColor="red"
-        handleBackgroundColor="white"
-      />
+    <div className="row CompareImage__container">
+      <div className="col-md-12 CompareImage__col">
+        <ImageSlider
+          image1={img1}
+          image2={img2}
+          customHandle={
+            <Button className="Compare__btn">
+              <ArrowLeft fontSize={30} />
+               <ArrowRight fontSize={30} />
+            </Button>
+          }
+          sliderWidth={3}
+          sliderColor="#3ac280"
+          handleColor="#3ac280"
+          handleBackgroundColor="white"
+        />
+      </div>
     </div>
   );
 }
