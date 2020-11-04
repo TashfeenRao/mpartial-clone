49ad851a9094330893985c3d5cb6c79ac3901198
tashfeenrao <raotashfeen25@gmail.com>
2020-11-05 02:29:09 +0500

    footer style added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,27 @@
 import React from 'react'
+import '../../css/Footer.css'
 import ic1 from '../../images/facebook.png'
 import ic2 from '../../images/linkedin.png'
 import ic3 from '../../images/instagram.png'
 
 export default function Footer() {
   return (
-    <div className="container-fluid footer__container">
+    <footer className="container-fluid footer__container">
       <div className="row">
-        <div className="col-md-6 col-lg-12">
-          <p>
+        <div className="col-md-12 col-lg-6">
+          <p className="footer__text">
             mpartial, a 4D Schematics Company. Copyright 2020 all rights
             reserved.
           </p>
         </div>
-      <div className="col-md-6 col-lg-12">
-      <div className="footer__social">
-      <img src={ic1} alt="" />
-      <img src={ic2} alt="" />
-      <img src={ic3} alt="" />
+        <div className="col-md-6 col-lg-6">
+          <div className="footer__social">
+            <img src={ic1} alt="" />
+            <img src={ic2} alt="" />
+            <img src={ic3} alt="" />
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-    </div>
+    </footer>
   );
 }
