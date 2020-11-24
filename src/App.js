41622a3606543a4ae383dd45341c5d03bc32e9a9
tashfeenrao <raotashfeen25@gmail.com>
2020-11-25 04:58:00 +0500

    deleted previous files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import Banner from "./components/Banner/Banner";
-import ContactFrom from "./components/ContactForm/ContactFrom";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import HowItWork from "./components/HowItWork/HowItWork";
-import SectionInfo from "./components/SectionInfo/SectionInfo";
-import VideosComp from "./components/VideosComp/VideosComp";
-import WaterFall from "./components/WaterFall/WaterFall";
-
 function App() {
-  return (
-    <>
-      <Header />
-      <Banner />
-      <SectionInfo />
-      <HowItWork />
-      <WaterFall />
-      <VideosComp />
-      <ContactFrom />
-      <Footer />
-    </>
-  );
+  return <h1>Hello React</h1>;
 }
 
 export default App;
